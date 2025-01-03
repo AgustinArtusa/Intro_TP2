189cfb00ed488037208695bc4fdb6badbe4619f9
LeandroAvalosUrbano <lavalos@fi.uba.ar>
2025-01-03 02:21:53 -0300

    Put de usuarios

--- Backend/src/routes/usuarios.js
+++ Backend/src/routes/usuarios.js
@@ -58,4 +58,24 @@ router.delete('/:id', async (req, res) => {
     res.send(usuario)
 })
 
+
+router.put('/:id', async (req, res) => {
+    let usuario_index = usuarios.findIndex((element) => element.id == req.params.id)
+    if (usuario_index === -1){
+        res.sendStatus(404)
+        return
+    }
+
+    usuarios[usuario_index].nombre = (req.body.nombre !== null && req.body.nombre !== undefined) ? req.body.nombre : usuarios[usuario_index].nombre
+    usuarios[usuario_index].dinero = (req.body.dinero !== null && req.body.dinero !== undefined) ? req.body.dinero : usuarios[usuario_index].dinero
+
+    usuarios[usuario_index].telefono = (req.body.telefono !== null && req.body.telefono !== undefined) ? req.body.telefono : usuarios[usuario_index].telefono
+    usuarios[usuario_index].contraseña = (req.body.contraseña !== null && req.body.contraseña !== undefined) ? req.body.contraseña : usuarios[usuario_index].contraseña
+    usuarios[usuario_index].username = (req.body.username !== null && req.body.username !== undefined) ? req.body.username : usuarios[usuario_index].username
+
+    
+    res.send(usuarios[usuario_index])
+
+
+})
 module.exports = router
