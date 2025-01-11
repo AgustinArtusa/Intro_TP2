c29611f9dac52dd29ef693aae312a4567af818e1
LeandroAvalosUrbano <lavalos@fi.uba.ar>
2025-01-11 17:17:04 -0300

    se agrego Post

--- Backend/src/routes/tiendas.js
+++ Backend/src/routes/tiendas.js
@@ -9,7 +9,33 @@ router.get('/', async (req, res) => {
     res.json(tiendas);
 });
 
+router.get('/:id', async (req, res) => {
+    const tiendas = await prisma.tienda.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
 
+    if (tiendas === null){
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(tiendas)
+});
+
+router.post('/', async (req, res) => {
+    const tiendas = await prisma.tienda.create({
+        data: {
+            ubicacion: req.body.ubicacion,
+            horario: req.body.horario,
+            estado: req.body.estado,
+            telefono: req.body.telefono,
+            email_contacto: req.body.email_contacto
+        }
+    })
+    res.status(201).send(tiendas)
+});
 
 
 module.exports = router;
