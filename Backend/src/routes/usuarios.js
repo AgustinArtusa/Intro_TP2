d6c66215f6db0e8e07e8ca98e6a446a4a2a4c9b3
Ivan <inovoa@fi.uba.ar>
2025-01-17 12:12:48 -0300

    Muevo los metodos POST y GET de compras hacia usuarios.js, y elimino compras.js

--- Backend/src/routes/usuarios.js
+++ Backend/src/routes/usuarios.js
@@ -99,4 +99,71 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.get('/:id/articulos', async (req, res) => {
+    const compras = await prisma.compra.findMany({
+        include: {
+            usuario: true,
+            articulo: true,
+        },
+    }).catch(error => {
+        res.status(500).json({error: `Error al obtener las compras: ${error.message}`});
+    });
+    
+    if (!compras){
+        return res.status(404).json({message: 'No se encontraron compras'})
+    }
+    res.json(compras)
+})
+
+router.post('/:id/articulos', async (req,res) => {
+    const usuarioId = parseInt(req.params.id);
+    const { articuloId } = req.body;
+    
+    if (!articuloId) {
+        return res.status(400).json({message: 'Se requiere articuloId.'});
+    }
+
+    const disponibilidad = await prisma.disponibilidad.findFirst({
+        where: {
+            articuloId: articuloId,
+            disponible: true,
+        },
+    });
+
+    if (!disponibilidad) {
+        return res.status(404).json({message: 'El articulo no está disponible'})
+    }
+
+    const usuario = await prisma.usuario.findUnique({
+        where: { id: usuarioId },
+    });
+    const articulo = await prisma.articulo.findUnique({
+        where: { id: articuloId },
+    });
+
+    if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado.' });
+    }
+    if (!articulo) {
+        return res.status(404).json({ message: 'Artículo no encontrado.' });
+    }
+    if (usuario.dinero < articulo.precio) {
+        return res.status(400).json({ message: 'No hay suficiente dinero para comprar este artículo.' });
+    }
+
+    const compra = await prisma.compra.create({
+        data: {
+            usuarioId: usuarioId,
+            articuloId: articuloId,
+        },
+    });
+
+    await prisma.usuario.update({
+        where: { id: usuarioId },
+        data: { dinero: usuario.dinero - articulo.precio },
+    });
+
+    res.status(201).json(compra);
+})
+
 module.exports = router
