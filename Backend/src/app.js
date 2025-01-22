ad2d152fdc4336f2791f62089253ca4a17b0740d
Agustin <aartusa@fi.uba.ar>
2025-01-22 14:02:49 -0300

    Se añadio express-sessions a app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,19 @@ var cors = require('cors')
 const usuarios = require('./routes/usuarios')
 const articulos = require('./routes/articulos')
 const tiendas = require('./routes/tiendas')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(cors())
+
+app.use(session({
+  secret: 'contraseña123',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => {
   res.send('Pagina inicio')
